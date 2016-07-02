d54cd51eea3d5ab75c63d2ffd1add7d64d079db5
Gordon Portzline <gordlonious@gmail.com>
2016-07-02 07:54:26 -0400

    new arrayMax helper

---
+++ binMax.cpp
@@ -0,0 +1,6 @@
+#ifndef binMax_S
+#define binMax_S
+
+enum binMax { alif = 2, ba = 4, ta = 8, tha = 16, gim = 32, how = 64, haak = 128, dal = 256, dhal = 512 } mBit;
+
+#endif
---
+++ binMax.h
@@ -0,0 +1,7 @@
+#ifndef binMax_H
+#define binMax_H
+// post name ( something to instantiate??? ) causes errors
+// EX: enum binMax { alif = 2, ba = 4, ta = 8, tha = 16, gim = 32, how = 64, haak = 128, dal = 256, dhal = 512 } mBits;
+enum binMax { alif = 2, ba = 4, ta = 8, tha = 16, gim = 32, how = 64, haak = 128, dal = 256, dhal = 512 };
+
+#endif
